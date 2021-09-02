2b8ef254000f0a4c1ce7e430ed79761bd2b3f648
BubuDavid <david.pedroza.segoviano@gmail.com>
2021-09-02 15:21:22 -0500

    Add some index page styles

--- static/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const hi = document.getElementById('Hello-World')
-let x = 0;
-hi.style.position = 'absolute';
-
-setInterval(() => {
-    x++;
-    hi.style.left = `${x}px`;
-    hi.style.top = `${x}px`;
-}, 50);
