4cda15e3e56ec1bfcb07a0b922deeb3915a6ae6b
BubuDavid <david.pedroza.segoviano@gmail.com>
2021-09-03 19:17:55 -0500

    Delete unnecessary files and fix design bug

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,15 @@
-const header = document.querySelector('.header')
+const header = document.querySelector(".header");
+const profile_img = document.getElementById("profile_image");
+const page_url = window.location.href;
 
-header.addEventListener('click', () => {
-    window.location.replace("/");
-})
+// Redirect
+header.addEventListener("click", () => {
+  window.location.replace("/");
+});
+
+// Make the profile image to go away
+if (page_url.includes("songs-page")) {
+  setInterval(() => {
+    profile_img.classList.add("hidden");
+  }, 500);
+}
